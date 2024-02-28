fad45d49300a7c70c17023dee14cf8b05783dff7
M. SHOZAUL BARY <rabby16139@gmail.com>
2024-02-28 23:43:42 +0800

    database have gone to data.

--- Backhand/controllors/regController.js
+++ Backhand/controllors/regController.js
@@ -1,4 +1,7 @@
-const regController = (req, res) => {
+const User = require("../modle/userModle");
+const bcrypt = require("bcrypt");
+
+const regController = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.send({ error: "Plese fill up the from" });
@@ -6,9 +9,22 @@ const regController = (req, res) => {
   if (password && password.length < 6) {
     return res.send({ error: "possoword too small" });
   }
-
-  console.log("databasee datta");
-
-  // res.send(data);
+  const existingUser = await User.find({ email: email });
+  if (existingUser.length > 0) {
+    return res.send({ error: `${email} alrady in use` });
+  } else {
+    bcrypt.hash(password, 10, function (err, hash) {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hash,
+      });
+      user.save();
+      res.send({
+        name: user.name,
+        email: user.email,
+      });
+    });
+  }
 };
 module.exports = regController;
